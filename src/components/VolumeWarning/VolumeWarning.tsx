4cb8e89a5b3f4fed839c4774d960eb53a2e6973b
I. Leonov <dumndunm@gmail.com>
2022-02-20 15:58:21 +0300

    New text

--- src/components/VolumeWarning/VolumeWarning.tsx
+++ src/components/VolumeWarning/VolumeWarning.tsx
@@ -4,10 +4,10 @@ import './VolumeWarning.css';
 const VolumeWarning: FC = () => {
   return (
     <span className="volume-warning">
-      The app works with sound, please turn down the volume of your speakers or
-      other audio device. If you are sure that you are ready to continue using,
-      just <span className="volume-warning__accent-word">click</span> anywhere
-      on the screen.
+      The app works with sound. Make sure your ears won't be affected by
+      forceful audio signals. If you are ready to continue, just
+      <span className="volume-warning__accent-word"> click</span> anywhere to
+      start.
     </span>
   );
 };
