fc607ff6d2e1d0553e0240dbc36f010393801c0c
I. Leonov <dumndunm@gmail.com>
2022-04-11 22:08:04 +0300

    Context menu refctor

--- src/components/ContextMenu/ContextMenu.tsx
+++ src/components/ContextMenu/ContextMenu.tsx
@@ -2,32 +2,54 @@ import React, { FC } from 'react';
 import b_ from 'b_';
 import './ContextMenu.css';
 import { isEmptyArray } from 'utils/isEmpty';
-import { ContextMenuPropsT } from './ContextMenu.models';
+import { ContextMenuItemT, ContextMenuPropsT } from './ContextMenu.models';
+import { NodeCategories, NodeTypes } from 'components/Nodes/models';
 import { contextMenuData } from 'data/contextMenu';
 
 const b = b_.with('context-menu');
 
+const Item: FC<{
+  category: NodeCategories;
+  label: string;
+  nodeType: NodeTypes;
+  onChooseItem: (type: NodeTypes, category: NodeCategories) => void;
+}> = ({ category, label, nodeType, onChooseItem }) => (
+  <li
+    key={label}
+    className={b('item')}
+    onClick={() => onChooseItem(nodeType, category)}
+  >
+    <span>{label}</span>
+  </li>
+);
+
+const Category: FC<{
+  category: NodeCategories;
+  items: Array<ContextMenuItemT>;
+  onChooseItem: (type: NodeTypes, category: NodeCategories) => void;
+}> = ({ category, items, onChooseItem }) => (
+  <li key={category} className={b('item')}>
+    <span>{category}</span>
+    <span className={b('arrow')}>‣</span>
+    {!isEmptyArray(items) && (
+      <ul className={b({ sub: true })}>
+        {items.map(({ label, nodeType }) => (
+          <Item
+            category={category}
+            label={label}
+            nodeType={nodeType}
+            onChooseItem={onChooseItem}
+          />
+        ))}
+      </ul>
+    )}
+  </li>
+);
+
 const ContextMenu: FC<ContextMenuPropsT> = ({ addNode }) => (
   <ul className={b()}>
     {contextMenuData.map(({ category, items }) => (
-      <li key={category} className={b('item')}>
-        <span>{category}</span>
-        <span className={b('item-arrow')}>‣</span>
-
-        {!isEmptyArray(items) && (
-          <ul className={b('sub-menu')}>
-            {items.map(({ label, nodeType }) => (
-              <li
-                key={label}
-                className={b('item')}
-                onClick={() => addNode(nodeType, category)}
-              >
-                <span>{label}</span>
-              </li>
-            ))}
-          </ul>
-        )}
-      </li>
+      <Category category={category} items={items} onChooseItem={addNode} />
     ))}
   </ul>
 );
